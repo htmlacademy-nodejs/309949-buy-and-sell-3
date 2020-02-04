49723858d7233725d30ad1167141d3a30d567d2a
Ivan Savitsky <ivan@savitsky.biz>
2020-02-04 22:36:22 +0300

    Add first server

--- src/service/cli/constants.js
+++ src/service/cli/constants.js
@@ -2,6 +2,7 @@
 
 module.exports.DEFAULT_COUNT = 1;
 module.exports.FILE_NAME = `mocks.json`;
+module.exports.DEFAULT_PORT = 3000;
 
 module.exports.FilePath = {
   sentences: `./data/sentences.txt`,
@@ -22,4 +23,12 @@ module.exports.SumRestrict = {
 module.exports.PictureRestrict = {
   min: 0,
   max: 16
+};
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };

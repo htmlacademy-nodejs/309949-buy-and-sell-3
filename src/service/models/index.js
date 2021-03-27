ea164f4ed4371217fb7c1bfd4672e44bf178493c
Ivan Savitsky <ivan@savitsky.biz>
2021-03-27 15:46:15 +0300

    Add models

---
+++ src/service/models/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const {Model} = require(`sequelize`);
+
+const defineCategory = require(`./category`);
+const defineComment = require(`./comment`);
+const defineOffer = require(`./offer`);
+
+const Alias = require(`./aliases`);
+
+const define = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Offer = defineOffer(sequelize);
+
+  Offer.hasMany(Comment, {as: Alias.COMMENTS, foreignKey: `offerId`});
+  Comment.belongsTo(Offer, {foreignKey: `offerId`});
+
+  Offer.belongsToMany(Category, {through: `offerCategories`, as: Alias.CATEGORIES});
+  Category.belongsToMany(Offer, {through: `offerCategories`, as: Alias.OFFERS});
+
+  class OfferCategory extends Model {}
+  OfferCategory.init({}, {sequelize});
+
+  Offer.belongsToMany(Category, {through: OfferCategory, as: Alias.CATEGORIES});
+  Category.belongsToMany(Offer, {through: OfferCategory, as: Alias.OFFERS});
+  Category.hasMany(OfferCategory, {as: Alias.OFFER_CATEGORIES});
+
+  return {Category, Comment, Offer, OfferCategory};
+};
+
+module.exports = define;
